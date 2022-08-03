c726a8e2ebed16854314889709f1c1d0154730bf
Daria <101130753+DS2905@users.noreply.github.com>
2022-08-03 12:22:03 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,8 +2,6 @@
 #pip install pytest-selenium
 #pytest -v --driver Chrome --driver-path C:\Users\0\Desktop\25.5\app\chromedriver.exe
 import pytest
-from settings import valid_password
-from settings import valid_email
 from selenium import webdriver
 
 @pytest.fixture(autouse=True)
